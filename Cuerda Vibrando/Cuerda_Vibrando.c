0d687ec13d8b70f08828d51ee1063bd2425dcfdb
JuanDiegoArango <arangojuan1993@gmail.com>
2014-11-02 08:56:41 -0500

    un poco más de cuerda

--- Cuerda Vibrando/Cuerda_Vibrando.c
+++ Cuerda Vibrando/Cuerda_Vibrando.c
@@ -1,30 +1,37 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-void condicion_inicial(float *y_pasado, int particion, float L, float pasos)
+void condicion_inicial(float *y_pasado, int particion, float L, float delta_x);
+void el_tiempo_avanza(float *y_pasado, float *y_presente, float *y_futuro);
+void  evolucion_en_el_timepo(float *y_futuro, float *y_pasado, float *y_presente,int t,int particion,float r);
+
+
 
 int main(int argc, char *argv[])
 
 {
 float L;
 float T;
-float t;
+int t;
 int i;
+float rho;
 int particion = 10000*L;
-float pasos=L/particion;
-float *x,*y_pasado,*y_futuro;
-x=malloc(pasos*sizeof(float));
-y_pasado=malloc(pasos*sizeof(float));
-y_futuro=malloc(pasos*sizeof(float));
-condicion_inicial(y_pasado,particion,L,pasos);
+float delta_x=L/particion;
+float delta_t=0.00005;
+float *x,*y_pasado,*y_futuro,*y_presente;
+float r=(delta_t/delta_x)*sqrt(T/rho);
+x=malloc(delta_x*sizeof(float));
+y_pasado=malloc(delta_x*sizeof(float));
+y_presente=malloc(delta_x*sizeof(float));
+y_futuro=malloc(delta_x*sizeof(float));
+    
+condicion_inicial(y_pasado,particion,L,delta_x);
     
     
-for (i=1; i<particion; i++)
+for (i=1; i<particion-1; i++)
+
 {
-  
-    
-    
-    
+    y_presente[i]=y_pasado[i]+pow(r,2)/2*(y_pasado[i+1]-2.0*y_pasado[i]+y_pasado[i-1])
     
     
 }
@@ -38,20 +45,20 @@ return 0;
     
 }
 
-void condicion_inicial(float *y_pasado, int particion, float L, float pasos){
+void condicion_inicial(float *y_pasado, int particion, float L, float delta_x){
     int i=0;
     
     for (i=1; i<particion; i++) {
         if (i<=0.8*L)
         {
-            y_pasado[i]=1.25*(pasos*i/L)
+            y_pasado[i]=1.25*(delta_x*i/L)
             
         }
         
         
         if (i>0.8*L)
         {
-            y_pasado[i]=5-5*(pasos*i/L)
+            y_pasado[i]=5-5*(delta_x*i/L)
            
         }
         
@@ -60,7 +67,50 @@ void condicion_inicial(float *y_pasado, int particion, float L, float pasos){
         
     }
 
-
+    
+    void  evolucion_en_el_timepo(float *y_futuro, float *y_pasado, float *y_presente,int t,int particion,float r)
+    
+    
+    { int i:
+      int j;
+     for(i=0,i<t,i++){
+         
+         for (j=1;j<particion-1; j++) {
+             
+             y_futuro[j]=2*(1-r*r)*y_presente[j]-y_pasado+[j]+r*r(y_presente[j+1]+y_presente[j-1])
+             
+         }
+         
+         el_tiempo_avanza(y_pasado, y_presente, y_futuro,particion);
+     
+     }
+    
+    
+    
+    
+    
+    
+    }
+    
+    
+    
+    void el_tiempo_avanza(float *y_pasado, float *y_presente, float *y_futuro,int particion)
+    {
+        int i;
+        
+        for (i=0; i<particion; i++)
+        {   y_pasado[i]=y_presente[i];
+            y_presente[i]=y_futuro[i];
+            
+        }
+    
+    
+    
+    
+    }
+    
+    
+    
 
 
 
