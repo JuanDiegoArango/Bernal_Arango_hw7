91a9435b40541e52d698e7f3c8ee7bcd1edee734
JuanDiegoArango <arangojuan1993@gmail.com>
2014-11-11 10:36:19 -0500

    casi terminamos

--- Shock/shock.c
+++ Shock/shock.c
@@ -2,20 +2,22 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-
+//Este codigo corre el caso de 2 gases ideales en un tubo inicialmente separado por una membrana y su evolucion en el tiempo. Se ha verificado este codigo al correrlo a 0.01s obteniendo perfiles de densidad, velocidad y presion equivalentes a los obtenidos analiticamente y reportados en la pagina http://nbviewer.ipython.org/github/numerical-mooc/numerical-mooc/blob/master/lessons/03_wave/03_05_Sods_Shock_Tube.ipynb
 int main(int argc, char **argv){
-  double rhoL=1.0;
-  double vL=0.0;
-  double pL=100000.0;
-  double rhoR=0.125;
-  double vR=0.0;
-  double pR=10000.0;
-  double x0=-10.0;
-  double xf=10.0;
-  double gamma=1.4;
-  double dt=0.00001;
-  int N=1000;
+    //Definicion de constantes del problema
+  double rhoL=1.0;      //Densidad a la izquierda
+  double vL=0.0;        //Velocidad a la izquierda
+  double pL=100000.0;   //Presion a la izquierda
+  double rhoR=0.125;    //Densidad a la derecha
+  double vR=0.0;        //Velocidad a la derecha
+  double pR=10000.0;    //Presion a la derecha
+  double x0=-10.0;      //Longitud inicial
+  double xf=10.0;       //Longitud final
+  double gamma=1.4;     //Coeficiente adiabatico
+  double dt=0.00001;    //Discretizacion en tiempo
+  int N=1000;           //Numero de discretizaciones en espacio
   int i,j;
+    //Vectores que almacenan las variables x, u1, u2, u3, f1, f2, f3, rho, v y P para estados iniciales, finales y medios (utilizados en el metodo de Richtmyer)
   double x[N];
   double f1_initial[N];
   double f1_past[N];
@@ -44,14 +46,15 @@ int main(int argc, char **argv){
   FILE *fileout;
   double tf;
 
-  tf=atof(argv[1]);
+  tf=atof(argv[1]);     // Tiempo final ingresado por consola
 
-  int Ntime=tf/dt;
+  int Ntime=tf/dt;      //Discretizacion en tiempo
   double u1_future[N];
   double u2_future[N];
   double u3_future[N];
   double dx;
   dx=(xf-x0)/(N-1.0);
+    //Definicion de las condiciones iniciales
   for(i=0;i<N;i++){
     x[i]=x0+i*dx;
     if(x[i]<0){
@@ -70,7 +73,8 @@ int main(int argc, char **argv){
 
   }
 
-  for(j=0;j<Ntime-1;j++){    
+  for(j=0;j<Ntime-1;j++){
+      //Fijar los valores extremos dado que no se calculan por el metodo de Richtmyer
     u1_future[0]=rho_initial[0];
     u2_future[0]=rho_initial[0]*v_initial[0];
     u3_future[0]=(p_initial[0]/(gamma-1)+rho_initial[0]*v_initial[0]*v_initial[0]/2.0);   
@@ -92,6 +96,7 @@ int main(int argc, char **argv){
       f3_half[i]=f3_initial[i];
     }
     for(i=1;i<(N-1);i++){
+        //Actualizacion de los valores para la siguiente iteracion
       u1_half[i]=(u1_initial[i+1]+u1_initial[i])/2.0-(f1_initial[i+1]-f1_initial[i])*dt/(2.0*dx);
       u2_half[i]=(u2_initial[i+1]+u2_initial[i])/2.0-(f2_initial[i+1]-f2_initial[i])*dt/(2.0*dx);
       u3_half[i]=(u3_initial[i+1]+u3_initial[i])/2.0-(f3_initial[i+1]-f3_initial[i])*dt/(2.0*dx);
@@ -103,13 +108,13 @@ int main(int argc, char **argv){
       u3_future[i]=u3_initial[i]-(f3_half[i]-f3_half[i-1])*dt/(dx);
     }
   }
-
+//Calculo de propiedades al final
   for(i=0;i<N;i++){
     rho_final[i]=u1_future[i];
     v_final[i]=u2_future[i]/rho_final[i];
     p_final[i]=(gamma-1)*(u3_future[i]-u2_future[i]*u2_future[i]/(2.0*u1_future[i]));
   }
-
+//Definicion de nombre de archivo de salida
   char name[10];
   sprintf(name, "_%f",tf);
   char new_name[20], end_name[10];
@@ -117,7 +122,7 @@ int main(int argc, char **argv){
   strcpy(end_name,".dat");
   strcat(new_name, name);
   strcat(new_name, end_name);
-
+//Impresion de los valores en el archivo de datos
   fileout=fopen(new_name,"w");
   for(i=0;i<N;i++){
     fprintf(fileout,"%f %f %f %f\n",x[i],v_final[i],p_final[i],rho_final[i]);
